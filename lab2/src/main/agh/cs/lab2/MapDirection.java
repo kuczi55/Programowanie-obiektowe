49be16a33f5e3480a837b3f7e4097cb6378ee605
kuczi55 <44415389+kuczi55@users.noreply.github.com>
2019-10-14 19:41:55 +0200

    Add files via upload

---
+++ lab2/src/main/agh/cs/lab2/MapDirection.java
@@ -0,0 +1,49 @@
+package agh.cs.lab2;
+
+enum MapDirection {
+    NORTH,
+    SOUTH,
+    WEST,
+    EAST;
+
+    public String toString(){
+        switch(this) {
+            case NORTH: return "Polnoc";
+            case SOUTH: return "Poludnie";
+            case WEST: return "Zachod";
+            case EAST: return "Wschod";
+        }
+        return null;
+    }
+
+    public MapDirection next(){
+        switch(this) {
+            case EAST: return SOUTH;
+            case SOUTH: return WEST;
+            case WEST: return NORTH;
+            case NORTH: return EAST;
+        }
+        return null;
+    }
+
+    public MapDirection previous(){
+        switch(this) {
+            case EAST: return NORTH;
+            case NORTH: return WEST;
+            case WEST: return SOUTH;
+            case SOUTH: return EAST;
+        }
+        return null;
+    }
+
+    public Vector2d toUnitVector() {
+        switch(this) {
+            case EAST: return new Vector2d(1,0);
+            case NORTH: return new Vector2d(0,1);
+            case WEST: return new Vector2d(-1, 0);
+            case SOUTH: return new Vector2d(0, -1);
+        }
+        return null;
+    }
+}
+
